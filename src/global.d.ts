0afdf6cd2ed8757771a348026c75eccf6a244e15
hecrean <hector@hectorcrean.xyz>
2022-08-03 16:26:08 +0100

    configuring types etc.

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+declare namespace svelte.JSX {
+  import { PointerDifference } from "@/lib/pannable";
+
+  interface HTMLAttributes<T> {
+    onpanstart?: (e: CustomEvent<{ x: number; y: number }>) => void;
+    onpanmove?: (
+      e: CustomEvent<{ x: number; y: number; dx: number; dy: number }>
+    ) => void;
+    onpanend?: (e: CustomEvent<{ x: number; y: number }>) => void;
+    onmultipointerpanmove?: (e: CustomEvent<PointerDifference>) => void;
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  server: {
+    host: "127.0.0.1",
+  },
+});
